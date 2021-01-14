6a259e88888b8f6d682bb4d6a1e1d33e5ee645ce
SebastienProfesseur <bois.sebastien@cgmatane.qc.ca>
2021-01-14 15:28:59 -0500

    Add files via upload

---
+++ Messageur.py
@@ -0,0 +1,43 @@
+﻿# Votre nom
+# installer python en mode administrateur : https://www.python.org/downloads/
+# https://pypi.org/project/pyserial/
+# pip install pyserial
+# https://pypi.org/project/requests/
+# pip install requests
+# Déposer le script dans C:\Users\Etudiant
+#    - Modifier le port pour celui utilisé dans Arduino (COM7, COM3, etc.)
+#    - S'assurer que Arduino utilise le même baudrate
+#    - Ajuster le chemin de l'URL ainsi que les paramètres reçus par ce script
+# En cas de probleme verifier les logs dans C:\xampp\apache\logs
+
+import serial
+import time
+import requests
+
+entree = serial.Serial(
+    port='COM7',\
+    baudrate=9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,\
+    timeout=0)
+
+print("Connecté à : " + entree.portstr)
+
+
+listeLettres = []
+ligne = 1
+while True:
+    for octet in entree.read():
+        listeLettres.append(chr(octet))  
+        valeur = ''.join(str(lettre) for lettre in listeLettres) # Transforme un type tableau en chaîne de caractères
+
+        if chr(octet) == '\n':
+            print("Ligne " + str(ligne) + ': ' + valeur)
+            requests.post('http://localhost/service.meteo/ajouter-meteo.php', data={'luminosite':valeur})
+            listeLettres = []
+            ligne += 1
+            break
+
+
+entree.close()
